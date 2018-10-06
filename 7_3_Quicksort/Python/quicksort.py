d94db454f57909c027cfe4d480802d945ed1d595
Boris <barkas62@gmail.com>
2018-10-05 21:46:02 -0700

    random pivot in quicksort

--- 7_3_Quicksort/Python/quicksort.py
+++ 7_3_Quicksort/Python/quicksort.py
@@ -1,7 +1,7 @@
 import random
 
 def partition(x, b, e):
-    m = (b+e)//2 # random.randint(b,e-1) #pivot
+    m = random.randint(b,e-1) #pivot
     pivot = x[m]
     x[b],x[m] = x[m],x[b]
     m = b
@@ -21,7 +21,7 @@ def quicksort(x, b = 0, e = -1):
     quicksort(x, b, m)
     quicksort(x, m+1, e)
 
-x = [3,1,7,2,8,4,1,2,1]
+x = [3,1,7,2,8,4,1,2,1,1]
 quicksort(x)
 
 pass
