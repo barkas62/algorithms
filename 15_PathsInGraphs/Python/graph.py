6ff1824c61dbaf625d53f8d9cbc8fa654c017c6d
boris <boris@evernote.com>
2019-02-13 13:57:24 -0800

    15_PathsInGraphs: MinHeap in Dijkstra

--- 15_PathsInGraphs/Python/graph.py
+++ 15_PathsInGraphs/Python/graph.py
@@ -1,65 +1,112 @@
 from collections import deque
-import copy
+import sys
 
 class MinHeap:
+    def parent(i):
+        return (i - 1)//2
+
+    def lchild(i):
+        return 2*i + 1
+
+    def rchild(i):
+        return 2*i + 2
+
     def __init__(self, nodes):
         '''
-
         :param elems:
         '''
-        self.heap = nodes #copy.copy(nodes)
+        self.ref = { node : i for i, node in enumerate(nodes) }
+        self.heap = nodes
+        self.heap[0].dist = 0
+
+    def __len__(self):
+        return len(self.heap)
 
     def pop_min(self):
-        i_min = 0
-        for i in range(1,len(self.heap)):
-            if self.heap[i] < self.heap[i_min]:
-                i_min = i
-        return self.heap.pop(i_min)
+        min_node = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.ref[self.heap[0]] = 0
+        self.ref.pop(min_node)
+        self.heap.pop()
+        self.sift_down(0)
+        return min_node
 
     def change_priority(self, node, new_dist):
+        if node.dist == new_dist:
+            return
+        i = self.ref[node]
+        old_dist = self.heap[i].dist
+        self.heap[i].dist = new_dist
+        if old_dist > new_dist:
+            self.sift_up(i)
+        else:
+            self.sift_down(i)
         node.dist = new_dist
 
     def empty(self):
         return len(self.heap) == 0
 
-class Graph:
+    def sift_down(self, i):
+        while True:
+            i_dn = i
+            l_id = MinHeap.lchild(i)
+            if l_id < len(self) and self.heap[l_id] < self.heap[i]:
+                i_dn = l_id
+            r_id = MinHeap.rchild(i)
+            if r_id < len(self) and self.heap[r_id] < self.heap[i_dn]:
+                i_dn = r_id
+            if i_dn == i:
+                break
+            self.ref[self.heap[i]] = i_dn
+            self.ref[self.heap[i_dn]] = i
+            self.heap[i_dn], self.heap[i] = self.heap[i], self.heap[i_dn]
+            i = i_dn
 
+    def sift_up(self, i):
+        while i > 0:
+            ip = MinHeap.parent(i)
+            if self.heap[ip] > self.heap[i]:
+                self.ref[self.heap[i]] = ip
+                self.ref[self.heap[ip]] = i
+                self.heap[ip], self.heap[i] = self.heap[i], self.heap[ip]
+                i = ip
+            else:
+                break
+
+
+class Graph:
     class Edge:
-        def __init__(self, node1, node2, data = None):
+        def __init__(self, node1, node2, data=None):
             self.node1 = node1
             self.node2 = node2
-            self.data  = data
+            self.data = data
 
         def next_node(self, node):
             return self.node2 if node == self.node1 else self.node1
 
     class Node:
-        def __init__(self, idx, data = None):
+        def __init__(self, idx, data=None):
             self.edges = []  # edges to adjacent nodes
-            self.idx = idx   # idx in array of nodes
+            self.idx = idx  # idx in array of nodes
             self.data = data
             self.reset()
 
         def reset(self):
-            self.dist = -1
+            self.dist = sys.maxsize
             self.prev = None
 
         def __lt__(self, other):
-            if self.dist == -1 and other.dist == -1:
+            if self.dist == None and other.dist == None:
                 return False
-            elif self.dist == -1:
+            elif self.dist == None:
                 return False
-            elif other.dist == -1:
+            elif other.dist == None:
                 return True
             return self.dist < other.dist
 
-        def __eq__(self, other):
-            return self.data == other.data
-
-
     def __init__(self, directed = False, n_nodes = 0):
         self.directed = directed
-        self.nodes = [Graph.Node() for i in range(n_nodes)]
+        self.nodes = [Graph.Node() for _ in range(n_nodes)]
 
     def add_node(self, data = None):
         idx = len(self.nodes)
@@ -89,7 +136,7 @@ class Graph:
             node = Q.popleft()
             for edge in node.edges:
                 nxt_node = edge.next_node(node)
-                if nxt_node.dist == -1:
+                if nxt_node.dist == sys.maxsize:
                     nxt_node.dist = node.dist + 1
                     nxt_node.prev = node
                     Q.append(nxt_node)
@@ -106,7 +153,6 @@ class Graph:
 
     def dijkstra(self, source, dest):
         '''
-
         :param source:
         :param dest:
         :return:
@@ -119,7 +165,7 @@ class Graph:
             for edge in min_node.edges:
                 nxt_node = edge.next_node(min_node)
                 # relaxing
-                if nxt_node.dist == -1 or nxt_node.dist > min_node.dist + edge.data:
+                if nxt_node.dist > min_node.dist + edge.data:
                     nxt_node.prev = min_node
                     H.change_priority( nxt_node, min_node.dist + edge.data)
 
@@ -130,7 +176,6 @@ class Graph:
             path.append(node)
 
         return list(reversed(path))
-
 
 
 g = Graph()
