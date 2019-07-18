3b6627599010883d43a79c90463ee5c81f541276
boris <boris@evernote.com>
2019-07-18 11:37:32 -0700

    Test for stability is added in mergesort

--- 6_2_Mergesort/Python/mergesort.py
+++ 6_2_Mergesort/Python/mergesort.py
@@ -11,33 +11,55 @@ def mergesort( x ):
         return x
     # split into 2 halves
     split_idx = len(x)//2
-    x1 = x[:split_idx]
-    x2 = x[split_idx:]
 
     # recurse to sort both halves
-    x1 = mergesort(x1)
-    x2 = mergesort(x2)
+    x1 = mergesort(x[:split_idx])
+    x2 = mergesort(x[split_idx:])
 
     # merge x1 and x2
-    i = 0
-    while x1 and x2:
-        if x1[0] < x2[0]:
-            x[i] = x1.pop(0)
+    i = i1 = i2 = 0
+
+    while i1 < len(x1) and i2 < len(x2):
+        if x1[i1] < x2[i2]:
+            x[i] = x1[i1]
+            i1 += 1
         else:
-            x[i] = x2.pop(0)
+            x[i] = x2[i2]
+            i2 += 1
         i += 1
-    if x1:
-        x[i:] = x1
-    elif x2:
-        x[i:] = x2
+
+    if i1 < len(x1):
+        x[i:] = x1[i1:]
+    elif i2 < len(x2):
+        x[i:] = x2[i2:]
 
     return x
+
+
+import random
 
 x = [3,2,5,1,8,5,2,1]
 x = mergesort(x)
 
-# s = 'fhaknciwnc'
-# s = mergesort(s) Not working because s in unmutable => 'x[] =' does not work
+n_test = 10
+
+a = [random.randrange(0, n_test) for _ in range(2*n_test)]
+a_stable = [(val, i) for i, val in enumerate(a)]
+
+a_sorted = mergesort(a_stable)
+
+for i in range(len(a_sorted)-1):
+    if a_sorted[i][0] > a_sorted[i+1][0]:
+        print("Sort test failed!")
+    if a_sorted[i][0] == a_sorted[i+1][0]:
+        if a_sorted[i][1] > a_sorted[i+1][1]:
+            print('Stability test failed!')
+            break
+else:
+    print('Sort and Stability test OK')
+
+s = 'fhaknciwnc'
+s = mergesort(s) #Not working because s in unmutable => 'x[] =' does not work
 
 pass
 
