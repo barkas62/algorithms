88abfe1b9e4754d0588be41ceb154c77bfae7850
barkas62 <barkas62@gmail.com>
2020-09-01 13:13:17 -0700

    Weighted trees union; percolate debugged

--- 10_DisjointSet/Python/percolate.py
+++ 10_DisjointSet/Python/percolate.py
@@ -3,21 +3,21 @@ import random
 class DisSet:
     def __init__(self, n):
         self.arr = [i for i in range(n) ]
-        self.rank = [0] * n
+        self.weight = [1] * n
 
     def union(self, i, j):
         pi = self.find(i)
         pj = self.find(j)
-        if self.rank[pi] < self.rank[pj]:
+        if self.weight[pi] < self.weight[pj]:
             self.arr[pi] = pj
+            self.weight[pj] += self.weight[pi]
         else:
             self.arr[pj] = pi
-            if self.rank[pi] == self.rank[pj]:
-                self.rank[pi] += 1
+            self.weight[pi] += self.weight[pj]
 
     def find(self, i):
         if i != self.arr[i]:
-            self.arr[i] = self.find( self.arr[i] )
+            self.arr[i] = self.find(self.arr[i])
         return self.arr[i]
 
 
@@ -25,9 +25,9 @@ def get_neibs(i, N):
     neibs = []
     r = i // N
     c = i % N
-    if r > 0:
+    if c > 0:
         neibs.append(i-1)
-    if r < N-1:
+    if c < N-1:
         neibs.append(i+1)
     if r > 0:
         neibs.append(i-N)
@@ -36,26 +36,38 @@ def get_neibs(i, N):
     return neibs
 
 
-def is_percolate(grid, N):
-    N2 = N*N
-    ds = DisSet( N2+2 )
-    for i in range(N2):
-        if grid[i]:
-            if i < N:
-                ds.union(i, N2)
-            if i > N2 - N - 1:
-                ds.union(i, N2+1)
-            neibs = get_neibs(i, N)
-            for n in neibs:
-                if grid[n]:
-                    ds.union(i, n)
+def can_percolate(grid, n):
+    # n sites + source + sink
+    source_id = n*n
+    sink_id = n*n + 1
+    ds = DisSet(n*n+2)
+    # Connect first row to source (has index n*n)
+    for c in range(n):
+        if grid[c]:
+            ds.union(c, source_id)
+    # Connect left-to-right and to below
+    for r in range(n-1):
+        for c in range(n-1):
+            i = r*n + c
+            if grid[i]:
+                if grid[i+1]:  # right
+                    ds.union(i, i+1)
+                if grid[i+n]:  # below
+                    ds.union(i, i+n)
+    # Connect last row to sink (has index n*n+1)
+    for c in range(n*(n-1), n*n):
+        if grid[c]:
+            ds.union(c, sink_id)
 
-    return ds.find(N2) == ds.find(N2+1)
+    return ds.find(source_id) == ds.find(sink_id)
 
-grid = [1,0,1,0]
+grid = [1,0,0,1]
 
-#grid = [[1 if random.random() < p else 0 for _ in range(N)] for _ in range(N)]
-res = is_percolate(grid, 2)
+N = 100
+p = 0.58
+grid = [1 if random.random() < p else 0 for _ in range(N*N)]
+
+res = can_percolate(grid, N)
 
 pass
 
